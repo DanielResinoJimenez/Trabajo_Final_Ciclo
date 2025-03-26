db12e01e9908954a87d6b0af25d632a322ad47e0
Daniel Resino <daniel.resinojimenez@riberadeltajo.es>
2025-03-26 09:15:51 +0100

    Modelo Opinion

---
+++ BackEnd/src/database/models/Opinion.js
@@ -0,0 +1,38 @@
+const sequelize = require("../db");
+
+const { Model, DataTypes } = require("sequelize");
+
+class Opinion extends Model {}
+
+Opinion.init({
+    id_opinion: {
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+    },
+    id_usuario: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'usuario',
+            key: 'id_usuario'
+        },
+        allowNull: false,
+    },
+    opinion: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    valoracion: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    fecha: {
+        type: DataTypes.DATE,
+        allowNull: false,
+    }
+},{
+    sequelize,
+    modelName: "opinion",
+    tableName: "opinion",
+    timestamps: false
+})
