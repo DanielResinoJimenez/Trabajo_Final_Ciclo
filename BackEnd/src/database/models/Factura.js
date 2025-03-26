ed1316efce0abf24fd035f09e943331493b8dfaf
Daniel Resino <daniel.resinojimenez@riberadeltajo.es>
2025-03-26 09:12:43 +0100

    Modelo Factura

---
+++ BackEnd/src/database/models/Factura.js
@@ -0,0 +1,33 @@
+const sequelize = require("../db");
+
+const { Model, DataTypes } = require("sequelize");
+
+class Factura extends Model {}
+
+Factura.init({
+    id_factura: {
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+    },
+    id_pedido: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: Pedido, 
+          key: 'id_pedido',      
+        },
+    },
+    fecha: {
+        type: DataTypes.DATE,
+        allowNull: false,
+    },
+    total: {
+        type: DataTypes.DECIMAL,
+        allowNull: false,
+    },
+},{
+    sequelize,
+    modelName: "factura",
+    tableName: "factura",
+    timestamps: false
+})
