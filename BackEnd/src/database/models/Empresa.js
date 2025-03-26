7295e81386502763d1a1944228a319a951565daf
Daniel Resino <daniel.resinojimenez@riberadeltajo.es>
2025-03-26 09:20:47 +0100

    Modelo Empresa

---
+++ BackEnd/src/database/models/Empresa.js
@@ -0,0 +1,39 @@
+const sequelize = require ("../db");
+
+const {Model, DataTypes} = require("sequelize");
+
+class Empresa extends Model {}
+
+Empresa.init({
+    id_empresa: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+    },
+    nombre: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    direccion: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    telefono: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    propietario: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+},{
+    sequelize,
+    modelName: "empresa",
+    tableName: "empresa",
+    timestamps: false,
+});
