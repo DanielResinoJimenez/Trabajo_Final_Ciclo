4a1e2212f913acfeeea6d14236216c2ef08bf941
Daniel Resino <daniel.resinojimenez@riberadeltajo.es>
2025-03-26 10:04:13 +0100

    Modelo de relacione PedidoMaquina y PedidoProducto

---
+++ BackEnd/src/database/models/PedidoMaquina.js
@@ -0,0 +1,35 @@
+const sequelize = require("../db");
+
+const {Model, DataTypes} = require('sequelize');
+
+class PedidoMaquina extends Model {}
+
+PedidoMaquina.init({
+    id_pedido_maquina:{
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+    },
+    id_pedido:{
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'pedido',
+            key: 'id_pedido',
+        },
+        allowNull: false,
+    },
+    id_maquina:{
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'maquina',
+            key: 'id_maquina',
+        },
+        allowNull: false,
+    },
+},{
+    sequelize,
+    modelName: 'pedido_maquina',
+    tableName: 'pedido_maquina',
+    timestamps: false,
+});
---
+++ BackEnd/src/database/models/PedidoProducto.js
@@ -0,0 +1,39 @@
+const sequelize = require("../db");
+
+const {Model, DataTypes} = require('sequelize');
+
+class PedidoProducto extends Model {}
+
+PedidoProducto.init({
+    id_pedido_producto:{
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+    },
+    id_pedido:{
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'pedido',
+            key: 'id_pedido',
+        },
+        allowNull: false,
+    },
+    id_producto:{
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'producto',
+            key: 'id_producto',
+        },
+        allowNull: false,
+    },
+    cantidad:{
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+},{
+    sequelize,
+    modelName: 'pedido_producto',
+    tableName: 'pedido_producto',
+    timestamps: false,
+});
