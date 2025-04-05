84b20b7857cb720831b537307bcc8a8a8a106ab7
Daniel Resino <thedan296@gmail.com>
2025-04-05 13:08:48 +0200

    Sin modificaciones importantes

--- FrontEnd/src/components/Layout.jsx
+++ FrontEnd/src/components/Layout.jsx
@@ -7,7 +7,7 @@ const Layout = () => {
   return (
     <div className='flex flex-col min-h-screen'>
         <Header />
-        <div className='z-0 flex-1'>
+        <div className='z-0 flex-1 mt-20'>
             <Outlet />
         </div>
         <Footer />
