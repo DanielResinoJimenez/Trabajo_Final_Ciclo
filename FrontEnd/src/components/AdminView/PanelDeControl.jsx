aeaebfde80f1acc1780ce75bc24a8fdf7ca5cefc
Daniel Resino <thedan296@gmail.com>
2025-04-22 18:15:13 +0200

    Control de apartados con botones en el panel de control

--- FrontEnd/src/components/AdminView/PanelDeControl.jsx
+++ FrontEnd/src/components/AdminView/PanelDeControl.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Link, Outlet } from 'react-router-dom'
+import { Link, Outlet, useNavigate } from 'react-router-dom'
 import usePanelControl from '../../services/hooks/usePanelControl'
 import maquinaIcono from './../../assets/images/maquina_panel_control.jpeg';
 import productoIcono from './../../assets/images/producto_panel_control.jpeg';
@@ -12,91 +12,89 @@ const PanelDeControl = () => {
 
     const [panelOpen, setPanelOpen] = useState(true);
 
+    const navigate = useNavigate(); // Hook para navegar programáticamente
+    const [mostrarOutlet, setMostrarOutlet] = useState(false); // Estado para manejar la visibilidad del <Outlet>
 
-    const openElement = () => {
-        if (panelOpen) {
-            let panel = document.getElementById('panelControl');
-            let volverPanel = document.getElementById('volverPanel');
-            panel.classList.add('hidden');
-            volverPanel.classList.remove('hidden');
-            setPanelOpen(false);
-
-        }
-    }
-
+    // Función para manejar el clic en el botón "Volver"
     const volverPanel = () => {
-        if (!panelOpen) {
-            let panel = document.getElementById('panelControl');
-            let outletContainer = document.getElementById('outlet_container');
-            let volverPanel = document.getElementById('volverPanel');
-            outletContainer.innerHTML = '';
-            volverPanel.classList.add('hidden');
-            panel.classList.remove('hidden');
-            setPanelOpen(true);
-        }
-    }
+        // Navegar a la ruta inicial (o a la ruta que quieras)
+        navigate('/panelControl'); // Redirige a la ruta principal
+        setMostrarOutlet(false); // Opcionalmente, puedes ocultar el outlet también
+    };
+
+    // Función para mostrar el <Outlet> si es necesario
+    const mostrarOutletFunc = () => {
+        setMostrarOutlet(true); // Mostrar el outlet si es necesario
+        const btnVolver = document.getElementById("volverPanel");
+        btnVolver.classList.remove("hidden"); // Mostrar el botón "Volver"
+    };
 
     return (
         <ProductosProvider>
             <MaquinasProvider>
                 <div>
-                    <button className='hidden fixed top-26 right-4 z-50 bg-green-500 hover:bg-green-700 text-white font-semibold px-5 py-3 rounded-full shadow-lg transition-all' id='volverPanel' onClick={volverPanel}>Volver</button>
-                    <div className='max-w-[1200px] max-lg:grid-cols-1 grid grid-cols-4 gap-4 m-auto p-10 panel__control rounded-lg shadow-lg shadow-yellow-950' id='panelControl'>
-                        <Link
-                            to={'productosAdmin'}
-                            className='relative max-lg:col-span-1 col-span-2 cursor-pointer bg-[#FDF8E5] flex justify-center items-center p-4 shadow-md rounded-4xl overflow-hidden transition-all duration-300 ease-in-out hover:shadow-lg hover:shadow-yellow-950 transform hover:-translate-y-2'
-                            onClick={openElement}
-                        >
-                            <span className='absolute top-5/6 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-yellow-950 text-[50px] font-semibold z-10'>
-                                Productos
-                            </span>
+                    <button onClick={volverPanel} className='hidden fixed top-26 right-4 z-50 bg-green-500 hover:bg-green-700 text-white font-semibold px-5 py-3 rounded-full shadow-lg transition-all' id='volverPanel'>Volver</button>
 
-                            <img
-                                src={productoIcono}
-                                className="w-[70%] object-contain opacity-50"
-                                alt="Icono Producto"
-                            />
-                        </Link>
-                        <Link
-                            to={'maquinasAdmin'}
-                            className='relative max-lg:col-span-1 col-span-2 cursor-pointer bg-[#98ADA6] flex justify-center items-center p-4 shadow-md rounded-4xl overflow-hidden transition-all duration-300 ease-in-out hover:shadow-lg hover:shadow-yellow-950 transform hover:-translate-y-2'
-                            onClick={openElement}
-                        >
-                            <span className='absolute text-white text-[50px] font-bold top-5/6 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10'>
-                                Máquinas
-                            </span>
-                            <img src={maquinaIcono} className="w-[70%] object-contain opacity-50" alt="Icono Máquina" />
-                        </Link>
+                    {!mostrarOutlet && (
+                        <div className='max-w-[1200px] max-lg:grid-cols-1 grid grid-cols-4 gap-4 m-auto p-10 panel__control rounded-lg shadow-lg shadow-yellow-950' id='panelControl'>
+                            <Link
+                                to={'productosAdmin'}
+                                className='relative max-lg:col-span-1 col-span-2 cursor-pointer bg-[#FDF8E5] flex justify-center items-center p-4 shadow-md rounded-4xl overflow-hidden transition-all duration-300 ease-in-out hover:shadow-lg hover:shadow-yellow-950 transform hover:-translate-y-2'
+                                onClick={mostrarOutletFunc}
+                            >
+                                <span className='absolute top-5/6 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-yellow-950 text-[50px] font-semibold z-10'>
+                                    Productos
+                                </span>
 
-                        <Link
-                            to={'cuentaAdmin'}
-                            className='relative max-lg:col-span-1 col-span-3 cursor-pointer bg-[#DEE3E6] flex justify-center items-center p-4 shadow-md rounded-4xl overflow-hidden transition-all duration-300 ease-in-out hover:shadow-lg hover:shadow-yellow-950 transform hover:-translate-y-2'
-                            onClick={openElement}
-                        >
-                            <span className='absolute text-gray-800 text-[50px] font-bold top-5/6 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10'>
-                                Cuenta
-                            </span>
-                            <img src={cuentaIcono} className="w-[40%] object-contain opacity-50" alt="Icono Cuenta" />
-                        </Link>
+                                <img
+                                    src={productoIcono}
+                                    className="w-[70%] object-contain opacity-50"
+                                    alt="Icono Producto"
+                                />
+                            </Link>
+                            <Link
+                                to={'maquinasAdmin'}
+                                className='relative max-lg:col-span-1 col-span-2 cursor-pointer bg-[#98ADA6] flex justify-center items-center p-4 shadow-md rounded-4xl overflow-hidden transition-all duration-300 ease-in-out hover:shadow-lg hover:shadow-yellow-950 transform hover:-translate-y-2'
+                                onClick={mostrarOutletFunc}
+                            >
+                                <span className='absolute text-white text-[50px] font-bold top-5/6 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10'>
+                                    Máquinas
+                                </span>
+                                <img src={maquinaIcono} className="w-[70%] object-contain opacity-50" alt="Icono Máquina" />
+                            </Link>
 
-                        <Link
-                            to={'solicitudesAdmin'}
-                            className='relative max-lg:col-span-1 col-span-1 cursor-pointer bg-[#FBDDB9] flex justify-center items-center p-4 shadow-md rounded-4xl overflow-hidden transition-all duration-300 ease-in-out hover:shadow-lg hover:shadow-yellow-950 transform hover:-translate-y-2'
-                            onClick={openElement}
-                        >
-                            <span className='absolute text-yellow-950 text-[50px] font-bold top-5/6 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 text-center'>
-                                Solicitudes
-                            </span>
-                            <img src={solicitudIcono} className='object-contain opacity-50' alt="Icono Solicitudes" />
-                        </Link>
-                        <Link to={'infoGeneralAdmin'} className='col-span-4 max-lg:col-span-1 cursor-pointer text-center p-10 bg-green-300 shadow-md rounded-4xl transition-all duration-300 ease-in-out hover:shadow-lg hover:shadow-yellow-950 transform hover:-translate-y-2' onClick={openElement}>
+                            <Link
+                                to={'cuentaAdmin'}
+                                className='relative max-lg:col-span-1 col-span-3 cursor-pointer bg-[#DEE3E6] flex justify-center items-center p-4 shadow-md rounded-4xl overflow-hidden transition-all duration-300 ease-in-out hover:shadow-lg hover:shadow-yellow-950 transform hover:-translate-y-2'
+                                onClick={mostrarOutletFunc}
+                            >
+                                <span className='absolute text-gray-800 text-[50px] font-bold top-5/6 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10'>
+                                    Cuenta
+                                </span>
+                                <img src={cuentaIcono} className="w-[40%] object-contain opacity-50" alt="Icono Cuenta" />
+                            </Link>
 
-                            <span className='text-[100px] max-md:text-[60px] font-bold h-full w-full text-yellow-950'>Información General</span>
+                            <Link
+                                to={'solicitudesAdmin'}
+                                className='relative max-lg:col-span-1 col-span-1 cursor-pointer bg-[#FBDDB9] flex justify-center items-center p-4 shadow-md rounded-4xl overflow-hidden transition-all duration-300 ease-in-out hover:shadow-lg hover:shadow-yellow-950 transform hover:-translate-y-2'
+                                onClick={mostrarOutletFunc}
+                            >
+                                <span className='absolute text-yellow-950 text-[50px] font-bold top-5/6 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 text-center'>
+                                    Solicitudes
+                                </span>
+                                <img src={solicitudIcono} className='object-contain opacity-50' alt="Icono Solicitudes" />
+                            </Link>
+                            <Link to={'infoGeneralAdmin'} className='col-span-4 max-lg:col-span-1 cursor-pointer text-center p-10 bg-green-300 shadow-md rounded-4xl transition-all duration-300 ease-in-out hover:shadow-lg hover:shadow-yellow-950 transform hover:-translate-y-2' onClick={mostrarOutletFunc}>
 
-                        </Link>
-                    </div>
+                                <span className='text-[100px] max-md:text-[60px] font-bold h-full w-full text-yellow-950'>Información General</span>
+
+                            </Link>
+                        </div>
+                    )}
+
+
                     <div id='outlet_container'>
-                        <Outlet />
+                        {mostrarOutlet && <Outlet />}
                     </div>
                 </div>
             </MaquinasProvider>
