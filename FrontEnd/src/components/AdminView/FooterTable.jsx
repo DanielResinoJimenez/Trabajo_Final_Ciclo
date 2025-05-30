f7bdac4cf2b090f679b326834afe47b47e412935
Daniel Resino <thedan296@gmail.com>
2025-05-30 18:26:30 +0200

    Cambio simple

--- FrontEnd/src/components/AdminView/FooterTable.jsx
+++ FrontEnd/src/components/AdminView/FooterTable.jsx
@@ -9,13 +9,14 @@ const FooterTable = ({ saldo, tipo }) => {
             {tipo === "Realizar" ? (
                 <tr id="fila-boton" className={`sticky bottom-0 text-black text-right z-10 ${saldo >= 0 ? "bg-green-500" : "bg-red-500"
                     }`}>
-                    <td><span
+                    <td colSpan={2}><span
                         onClick={nuevaAccion}
                         className="hover:bg-green-300 transition-colors cursor-pointer bg-green-200 px-20 py-2 border rounded-xl text-lg font-bold"
                     >
-                        +
+                        
+                        Realizar Movimiento
                     </span></td>
-                    <td></td>
+                    
                     <td></td>
                     {
                         tipo === "Realizar" && (<th></th>)
